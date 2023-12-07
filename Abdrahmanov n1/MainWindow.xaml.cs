accc9458289d072b8866bd0bcbe95c62b3684b7b
ArslanAbd <151536047+ArslanAbd@users.noreply.github.com>
2023-12-07 11:14:59 +0300

    Update MainWindow.xaml.cs

--- Abdrahmanov n1/MainWindow.xaml.cs
+++ Abdrahmanov n1/MainWindow.xaml.cs
@@ -27,10 +27,59 @@ namespace Abdrahmanov_n1
 
         private void BtnOK_Click(object sender, RountedEventArgs e)
 { 
-        Console.Write("Сторона квадрата: ");
-        int a = int.Parse(Console.ReadLine());
-        int p = 4 * a;
-        Console.Write("Периметр: " + p);
-        Console.ReadKey(true); 
-        
+        int b = Convert.ToInt32(TbNumberA.Text);
+            switch (b) // переключатель,основанный на выборе пользователя
+            {
+                case 1:
+                    {
+                        MessageBox.Show("понедельник"); // выбрал значение 1
+                        break;
+                    }
+
+                case 2:
+                    {
+                        MessageBox.Show("Вторник");    // выбрал значение 2
+                        break;
+                    }
+
+                case 3:
+                    {
+                        MessageBox.Show("Среда");      // выбрал значение 3
+                        break;
+                    }
+
+                case 4:
+                    {
+                        MessageBox.Show("Четверг");      // выбрал значение 4
+                        break;
+                    }
+
+                case 5:
+                    {
+                        MessageBox.Show("Пятница");      // выбрал значение 5
+                        break;
+                    }
+
+                case 6:
+                    {
+                        MessageBox.Show("Суббота");      // выбрал значение 6
+                        break;
+                    }
+
+                case 7:
+                    {
+                        MessageBox.Show("Воскресенье");   // выбрал значение 7
+                        break;
+                    }
+            }
+        }
+
+        private void TbNumberA_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
+ 
+
  }
